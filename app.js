39d920a674cd7f689da22538eb4ebff5ef071685
Allen Rosetti <cs@cleanpaw.com>
2018-09-07 13:43:11 -0700

    9/7/2018 - 1:43pm

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 var port = 3000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
 
 app.use("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
-
-// app.get("/", (req, res) => {
-//     res.send("hello world");
-// })
 
 app.listen(port, () => {
     console.log('Server listening on port ' +port);
